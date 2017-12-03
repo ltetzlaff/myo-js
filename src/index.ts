d55657a36945aaf7155c77ddc6c10c7df75f5c61
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2017-12-03 20:31:24 +0100

    :package: export submodules

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ import { MyoManager } from "./MyoManager"
 export const myoManager = new MyoManager()
 
 export default myoManager
+
+export * from "./MyoManager"
+export * from "./Myo"
+export * from "./types"
+export * from "./util"
